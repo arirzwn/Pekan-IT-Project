e8aaa9f3572a3f3624883f6a2877e537a5c44dfb
Arirzwn <aririzwan0@gmail.com>
2024-09-01 16:44:28 +0700

    update fitur

--- src/layout/TentangKami.jsx
+++ src/layout/TentangKami.jsx
@@ -1,8 +1,8 @@
-// src/layout/TentangKami.jsx
 import { useEffect } from "react";
 import Judul from "../components/Judul.jsx";
 import CardAnggota from "../components/CardAnggota.jsx";
 import Button from "../components/Button.jsx";
+import { motion } from "framer-motion";
 
 const timeline = [
   {
@@ -23,40 +23,25 @@ const timeline = [
   },
 ];
 
-export default function TentangKami() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+const variants = {
+  hidden: (direction) => ({
+    opacity: 0,
+    x: direction === "left" ? -100 : 100,
+  }),
+  visible: (i) => ({
+    opacity: 1,
+    x: 0,
+    transition: { delay: i * 0.3, duration: 0.5 },
+  }),
+};
 
-  return (
-    <>
-      <div className="mt-5">
-        <Judul className="mx-[80px]">Arisan's Journey</Judul>
-        <TimeLine />
-        <div className="mt-4 bg-[#FFFBE6] py-5 w-full">
-          <Judul className="mx-[80px]">Arisan's People</Judul>
-          <h4 className="mx-[80px]">Meet our team</h4>
-          <CardAnggota />
-        </div>
-        <Judul className="mt-5 text-center text-5xl font-light">
-          Let's Collaborate!!!
-        </Judul>
-        <div className="flex justify-center items-center h-full my-5">
-          <Button className="rounded-full bg-[#FF9100] text-white">
-            CONTACT US
-          </Button>
-        </div>
-      </div>
-    </>
-  );
-}
-
-function TimeLine() {
+function Timeline() {
   return (
     <div className="mx-auto w-[80%] relative">
       <div className="absolute left-1/2 transform -translate-x-1/2 top-[calc(1.25rem+1.25rem)] bottom-[calc(1.25rem+1.25rem)] border-l-2 border-gray-700"></div>
       <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
         {timeline.map((data, index) => {
+          const direction = index % 2 === 0 ? "left" : "right";
           let positionClass = "";
           if (data.title === "First Collaboration") {
             positionClass = "timeline-end";
@@ -67,10 +52,14 @@ function TimeLine() {
           }
 
           return (
-            <li key={data.tahun} className={`relative ${positionClass}`}>
-              {/*{index !== 0 && index !== timeline.length - 1 && (*/}
-              {/*  <hr className="border-t-2 border-gray-300 my-4 absolute left-1/2 transform -translate-x-1/2 w-[calc(50%-1.25rem)] top-1/2" />*/}
-              {/*)}*/}
+            <motion.li
+              key={data.tahun}
+              className={`relative ${positionClass}`}
+              custom={direction}
+              initial="hidden"
+              animate="visible"
+              variants={variants}
+            >
               <div className="timeline-middle relative z-10 bg-white p-1 rounded-full">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -92,10 +81,7 @@ function TimeLine() {
                 <div className="text-lg font-black">{data.title}</div>
                 {data.deskripsi}
               </div>
-              {/*{index !== timeline.length - 1 && (*/}
-              {/*  <hr className="border-t-2 border-gray-300 my-4 absolute left-1/2 transform -translate-x-1/2 w-[calc(50%-1.25rem)] bottom-1/2" />*/}
-              {/*)}*/}
-            </li>
+            </motion.li>
           );
         })}
       </ul>
@@ -103,46 +89,30 @@ function TimeLine() {
   );
 }
 
-// function TimeLine() {
-//   return (
-//     <div className="mx-auto w-[80%]">
-//       <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-//         {timeline.map((data, index) => {
-//           let positionClass = "";
-//           if (data.title === "First Collaboration") {
-//             positionClass = "timeline-end";
-//           } else if (data.title === "First Competition") {
-//             positionClass = "timeline-start";
-//           } else {
-//             positionClass = "timeline-start";
-//           }
-//
-//           return (
-//             <li key={data.tahun} className={positionClass}>
-//               {index !== 0 && <hr className="border-t-2 border-gray-300" />}
-//               <div className="timeline-middle">
-//                 <svg
-//                   xmlns="http://www.w3.org/2000/svg"
-//                   viewBox="0 0 20 20"
-//                   fill="currentColor"
-//                   className="h-5 w-5"
-//                 >
-//                   <path
-//                     fillRule="evenodd"
-//                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-//                     clipRule="evenodd"
-//                   />
-//                 </svg>
-//               </div>
-//               <div className={`timeline-content ${positionClass} mb-10 px-2`}>
-//                 <time className="font-mono italic">{data.tahun}</time>
-//                 <div className="text-lg font-black">{data.title}</div>
-//                 {data.deskripsi}
-//               </div>
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     </div>
-//   );
-// }
+export default function TentangKami() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <>
+      <div className="mt-5">
+        <Judul className="mx-[80px]">Arisan&apos;s Journey</Judul>
+        <Timeline />
+        <div className="mt-4 bg-[#FFFBE6] py-5 w-full">
+          <Judul className="mx-[80px]">Arisan&apos;s People</Judul>
+          <h4 className="mx-[80px]">Meet our team</h4>
+          <CardAnggota />
+        </div>
+        <Judul className="mt-5 text-center text-5xl font-light">
+          Let&apos;s Collaborate!!!
+        </Judul>
+        <div className="flex justify-center items-center h-full my-5">
+          <Button className="rounded-full bg-[#FF9100] text-white">
+            CONTACT US
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/Aktifitas.jsx
+++ src/pages/Aktifitas.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState, useRef } from "react";
+import { motion } from "framer-motion";
 import gambar1 from "/src/assets/gambar-1.png";
 import gambar2 from "/src/assets/gambar-2.png";
 import gambar3 from "/src/assets/gambar-3.png";
@@ -6,34 +8,75 @@ import gambar5 from "/src/assets/gambar-5.png";
 import gambar6 from "/src/assets/gambar-6.png";
 import gambar7 from "/src/assets/gambar-7.png";
 
+const variants = {
+  hidden: {
+    opacity: 0,
+    x: -100,
+  },
+  visible: (i) => ({
+    opacity: 1,
+    x: 0,
+    transition: { delay: i * 0.5, duration: 2 },
+  }),
+};
+
 export default function Aktifitas() {
+  const [inView, setInView] = useState(false);
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setInView(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.1 },
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (inView) {
+      document.body.style.overflowX = "hidden";
+    } else {
+      document.body.style.overflowX = "auto";
+    }
+  }, [inView]);
+
   return (
     <>
-      <div id="aktifitas">
+      <div id="aktifitas" ref={sectionRef}>
         <h1 className="my-10 font-bold text-2xl text-center">Aktivitas Kami</h1>
       </div>
-      <div className="  grid  grid-cols-4 grid-rows-3 mx-[70px] gap-3 ">
-        <figure className="col-span-3 h-[200px] overflow-hidden rounded-md ">
-          <img src={gambar1} className="" />
-        </figure>
-        <figure className="col-start-4 h-[200px]  rounded-md overflow-hidden">
-          <img src={gambar2} className="scale-110" />
-        </figure>
-        <figure className="col-span-2 row-start-2 h-[200px]  rounded-md  overflow-hidden">
-          <img src={gambar3} className="  " />
-        </figure>
-        <figure className="col-span-2 col-start-3 row-start -2 h-[200px]  rounded-md overflow-hidden">
-          <img src={gambar4} className="  " />
-        </figure>
-        <figure className="row-start-3 h-[200px]  rounded-md overflow-hidden  ">
-          <img src={gambar5} className="  " />
-        </figure>
-        <figure className="col-span-2 row-start-3 h-[200px]  rounded-md  overflow-hidden">
-          <img src={gambar6} className="  " />
-        </figure>
-        <figure className="col-start-4 row-start-3 h-[200px ] rounded-md  overflow-hidden">
-          <img src={gambar7} className="scale-110 " />
-        </figure>
+      <div className="grid grid-cols-4 grid-rows-3 mx-[70px] gap-3">
+        {[gambar1, gambar2, gambar3, gambar4, gambar5, gambar6, gambar7].map(
+          (gambar, index) => (
+            <motion.figure
+              key={index}
+              className={`h-[200px] overflow-hidden rounded-md ${index === 0 ? "col-span-3" : ""} ${index === 1 ? "col-start-4" : ""} ${index === 2 ? "col-span-2 row-start-2" : ""} ${index === 3 ? "col-span-2 col-start-3 row-start-2" : ""} ${index === 4 ? "row-start-3" : ""} ${index === 5 ? "col-span-2 row-start-3" : ""} ${index === 6 ? "col-start-4 row-start-3" : ""}`}
+              initial="hidden"
+              animate={inView ? "visible" : "hidden"}
+              variants={variants}
+              custom={index}
+            >
+              <img
+                src={gambar}
+                className={index === 1 || index === 6 ? "scale-110" : ""}
+              />
+            </motion.figure>
+          ),
+        )}
       </div>
     </>
   );
